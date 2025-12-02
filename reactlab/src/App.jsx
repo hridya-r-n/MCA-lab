20bd5a7119c40ecdfafdda344449cae832c92229
hridya-r-n <hridyarnair04@gmail.com>
2025-12-02 18:42:13 +0530

    commit

--- reactlab/src/App.jsx
+++ reactlab/src/App.jsx
@@ -2,23 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import StudentList from './list'
-import Counter from './counter'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+// import StudentList from './list'
+// import Counter from './counter'
+import Home from './home.jsx'
+import Contact from './contact.jsx'
+import About from './About.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>  
-      <div className="card">
-        {/* <StudentList/> */}
-        {/* <button onClick={() => setCount((count) => count + 1)}>
+     <div class='card'>
+    <BrowserRouter>
+   
+    <nav class='nav-bar'>
+      <a href='/'>Home</a> | <a href='/about'>About</a> | <a href='/contact'>Contact</a>
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+    
+    </BrowserRouter>
+    </div>
+       {/* <div className="card">
+         <StudentList/> 
+         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button> */}
+        </button> 
         <Counter/>
-      </div>
-      {/* <p className="read-the-docs">
+      </div> 
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>  */}
     </>
   )
 }
