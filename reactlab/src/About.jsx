2dd97283649401f5d81cce25ff80f76ad338cbf7
hridya-r-n <hridyarnair04@gmail.com>
2025-12-02 18:42:13 +0530

    commit

---
+++ reactlab/src/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const About=()=>{
+    return(
+        <div>This is a webpage demonstrating <b>Routing</b> and <b>Navigating</b> in React</div>
+    )
+}
+export default About
---
+++ reactlab/src/contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Contact=()=>{
+    return(
+        <div>
+            <h2>Phone: 9499875897</h2>
+            <h3>Email:abc@gmail.com</h3>
+        </div>
+    )
+}
+export default Contact
